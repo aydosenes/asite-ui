40c8d052f17530b7ef95da4cd0f52c61b031a188
Enes Aydos <aydosenes@outlook.com>
2025-03-23 23:32:35 +0300

    fix : config

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const { exec } = require("child_process");
+const express = require('express');
+const next = require('next');
 
-exec("npm start", (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error starting Next.js: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  const port = 3002;
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
 });
