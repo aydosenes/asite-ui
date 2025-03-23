a172e1a48e34108c1233f2bd279510f4d8e14c8c
Enes Aydos <aydosenes@outlook.com>
2025-03-23 22:20:05 +0300

    feat : config

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+      {
+        name: "asite-ui",
+        script: "npm",
+        args: "start",
+        cwd: "C:\\Users\\Administrator\\publish-2\\asite-ui",
+      }
+    ]
+  }
