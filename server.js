a43b474ca8b7e1bf6c89e0d702833f51ad153659
Enes Aydos <aydosenes@outlook.com>
2025-03-23 23:46:34 +0300

    fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.prepare().then(() => {
     return handle(req, res);
   });
 
-  server.listen(port, (err) => {
+  server.listen(port, '0.0.0.0', (err) => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
