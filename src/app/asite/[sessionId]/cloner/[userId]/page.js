7fae6d934a8244c8e29eaf01d53d6e42b0969b6c
Enes Aydos <aydosenes@outlook.com>
2025-09-30 13:01:23 +0300

    feat : cloner

---
+++ src/app/asite/[sessionId]/cloner/[userId]/page.js
@@ -0,0 +1,393 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import {
+  Typography,
+  Button,
+  Container,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+  Snackbar,
+  CircularProgress,
+  TextField,
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import PropTypes from "prop-types";
+import { use } from "react";
+
+export default function Asite({ params }) {
+  const unwrappedParams = use(params);
+  const [loading, setLoading] = useState(false);
+  const [allLoading, setAllLoading] = useState(false);
+  const [projectName, setProjectName] = useState("");
+  const [sourceProject, setSourceProject] = useState("");
+  const [targetProject, setTargetProject] = useState("");
+  const [projects, setProjects] = useState([]);
+  const [siteSets, setSiteSets] = useState([]);
+  const [token, setToken] = useState("");
+  const [importCompleted, setImportCompleted] = useState(false);
+  const urls = [
+    "revision-sets",
+    "roles",
+    "status-sets",
+    "metadata-sets",
+    "companies",
+    "company-groups",
+    "company-users",
+    "form-templates",
+  ];
+  useEffect(() => {
+    if (projects.length == 0) {
+      const token = localStorage.getItem("crane-token") || "";
+      if (token) {
+        setToken(token);
+        fetch(`${process.env.BASE_URL}/api/Cloner/get-projects`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ token }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            setProjects(data);
+          })
+          .catch((error) => router.push("/"));
+      }
+    }
+  }, []);
+
+  const handleSetSourceProject = async (e) => {
+    setSourceProject(e.target.value);
+    handleGetSiteSets(e.target.value);
+  };
+
+  const handleCreate = async () => {
+    try {
+      setLoading(true);
+      fetch(`${process.env.BASE_URL}/api/Cloner/create-project`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: projectName,
+          token: token,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setTargetProject(data.data);
+          setLoading(false);
+        })
+        .catch((error) => console.log(error));
+    } catch (err) {
+      setLoading(false);
+      console.error("Failed to get data:", err);
+    }
+  };
+
+  const handleGetSiteSets = async (id) => {
+    try {
+      setLoading(true);
+      fetch(`${process.env.BASE_URL}/api/Cloner/get-site-sets`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          projectId: id,
+          token: token,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setSiteSets(data);
+          setLoading(false);
+        })
+        .catch((error) => console.log(error));
+    } catch (err) {
+      setLoading(false);
+      console.error("Failed to get data:", err);
+    }
+  };
+
+  const handleExcelUpload = (index, file) => {
+    const newSiteSets = [...siteSets];
+    newSiteSets[index].excelFile = file;
+    setSiteSets(newSiteSets);
+    console.log(newSiteSets);
+  };
+  const handleRemoveExcel = (index) => {
+    setSiteSets((prev) =>
+      prev.map((s, i) => (i === index ? { ...s, excelFile: null } : s))
+    );
+  };
+
+  const handleImportSiteSets = async () => {
+    try {
+      setLoading(true);
+      for (const siteSet of siteSets) {
+        if (siteSet.excelFile) {
+          const formData = new FormData();
+          formData.append("ProjectId", targetProject.id);
+          formData.append("SiteSetName", siteSet.name);
+          formData.append("Token", token);
+          formData.append("File", siteSet.excelFile);
+          const response = await fetch(
+            `${process.env.BASE_URL}/api/Cloner/site-sets`,
+            {
+              method: "POST",
+              body: formData,
+            }
+          );
+          const result = await response.json();
+          console.log(result);
+          setImportCompleted(true);
+        }
+      }
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      console.error("Failed to import siteSets:", err);
+    }
+  };
+  const handleClone = async () => {
+    try {
+      setLoading(true);
+      setAllLoading(true);
+      for (const url of urls) {
+        fetch(`${process.env.BASE_URL}/api/Cloner/${url}`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            sourceProjectId: sourceProject,
+            targetProjectId: targetProject.id,
+            token: token,
+          }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+          })
+          .catch((error) => console.log(error));
+      }
+      setLoading(false);
+      setAllLoading(false);
+    } catch (err) {
+      setLoading(false);
+      setAllLoading(false);
+      console.error("Failed to clone:", err);
+    }
+  };
+
+  const messages = [
+    "Revision setting...",
+    "Roles setting...",
+    "Status setting...",
+    "Metadata setting...",
+    "Companies setting...",
+    "Company Groups setting...",
+    "Company Users setting...",
+    "Form Templates setting...",
+  ];
+  const [progress, setProgress] = useState(messages[0]);
+
+  useEffect(() => {
+    let index = 0;
+    const timer = setInterval(() => {
+      index = (index + 1) % messages.length;
+      setProgress(messages[index]);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  function CircularProgressWithLabel(props) {
+    return (
+      <Box sx={{ position: "relative", display: "flex" }}>
+        {/* <CircularProgress variant="determinate" {...props} /> */}
+        <CircularProgress size="30px"/>
+        <Box
+          sx={{
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            position: "absolute",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Typography
+            variant="caption"
+            component="div"
+            sx={{ color: "text.secondary" }}
+          >
+            {`${props.value}`}
+          </Typography>
+        </Box>
+      </Box>
+    );
+  }
+
+  CircularProgressWithLabel.propTypes = {
+    /**
+     * The value of the progress indicator for the determinate variant.
+     * Value between 0 and 100.
+     * @default 0
+     */
+    value: PropTypes.number.isRequired,
+  };
+
+  return (
+    <Container maxWidth="md" sx={{ pb: 20 }}>
+      <Typography variant="h6" sx={{ mb: 2 }}>
+        <CircularProgressWithLabel value={progress} />
+      </Typography>
+      <Box display="flex" gap={1} flexWrap="wrap" sx={{ mt: 1 }}>
+        <FormControl margin="normal" sx={{ flex: 1 }}>
+          <TextField
+            select
+            label="Source Project"
+            value={sourceProject}
+            onChange={handleSetSourceProject}
+            size="small"
+            sx={{ flex: 1 }}
+          >
+            {projects.map((s) => (
+              <MenuItem key={s.id} value={s.id}>
+                {s.name}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            label="Project Name"
+            variant="outlined"
+            size="small"
+            sx={{ flex: 1, mt: 2 }} // margin alignment için mt ekleyebilirsin
+            value={projectName}
+            onChange={(e) => setProjectName(e.target.value)}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            onClick={() => handleCreate()}
+            disabled={!projectName || loading}
+            sx={{ mt: "16px" }}
+          >
+            Create
+          </Button>
+        </FormControl>
+      </Box>
+
+      {sourceProject && targetProject && siteSets.length > 0 && (
+        <Box display="flex" gap={1} flexWrap="wrap" sx={{ mt: 1 }}>
+          <FormControl margin="normal" sx={{ flex: 1 }}>
+            <TableContainer component={Paper} sx={{ mt: 2 }}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Site Set Name</TableCell>
+                    <TableCell>File Name</TableCell>
+                    <TableCell>Action</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {siteSets.map((siteSet, index) => (
+                    <TableRow key={siteSet.id || index}>
+                      <TableCell align="left">
+                        <Typography
+                          sx={{ opacity: siteSet.excelFile ? 1 : 0.3 }}
+                        >
+                          {siteSet.name}
+                        </Typography>
+                      </TableCell>
+
+                      <TableCell
+                        sx={{
+                          maxWidth: 50, // sabit max genişlik
+                          whiteSpace: "nowrap", // tek satır
+                          overflow: "hidden", // taşanı gizle
+                          textOverflow: "ellipsis", // ... ile kısalt
+                        }}
+                      >
+                        {siteSet.excelFile ? siteSet.excelFile.name : "-"}
+                      </TableCell>
+                      <TableCell align="right">
+                        {siteSet.excelFile ? (
+                          <Button
+                            variant="outlined"
+                            color="error"
+                            size="small"
+                            onClick={() => handleRemoveExcel(index)}
+                          >
+                            Remove
+                          </Button>
+                        ) : (
+                          <Button
+                            variant="contained"
+                            component="label"
+                            size="small"
+                          >
+                            Upload
+                            <input
+                              type="file"
+                              accept=".xlsx, .xls"
+                              hidden
+                              onChange={(e) =>
+                                handleExcelUpload(index, e.target.files[0])
+                              }
+                            />
+                          </Button>
+                        )}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              onClick={() => handleImportSiteSets()}
+              disabled={
+                !siteSets.some((siteSet) => siteSet.excelFile !== undefined) ||
+                loading
+              }
+              sx={{ mt: "16px" }}
+            >
+              Import
+            </Button>
+          </FormControl>
+        </Box>
+      )}
+      {importCompleted && (
+        <Box display="flex" gap={1} flexWrap="wrap" sx={{ mt: 1 }}>
+          <FormControl margin="normal" sx={{ flex: 1 }}>
+            {allLoading ? (
+              <CircularProgressWithLabel value={progress} />
+            ) : (
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                onClick={() => handleClone()}
+                sx={{ mt: "16px" }}
+              >
+                Clone All Sets
+              </Button>
+            )}
+          </FormControl>
+        </Box>
+      )}
+    </Container>
+  );
+}
--- src/app/asite/[sessionId]/layout.js
+++ src/app/asite/[sessionId]/layout.js
@@ -1,6 +1,7 @@
 "use client";
 
 import * as React from "react";
+import { useState, useEffect } from "react";
 import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
@@ -14,6 +15,18 @@ import { Box, IconButton } from "@mui/material";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import { usePathname, useRouter } from "next/navigation";
+import LoginIcon from "@mui/icons-material/Login";
+import LogoutIcon from "@mui/icons-material/Logout";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  TextField,
+  DialogActions,
+  Button,
+  DialogContentText,
+} from "@mui/material";
 
 export default function DashboardLayout({ children }) {
   const router = useRouter();
@@ -38,50 +51,148 @@ export default function DashboardLayout({ children }) {
     router.push(newUrl);
   };
 
+  const handleProjectCloner = () => {
+    const newUrl = `/asite/${sessionId}/cloner/${userId}`;
+    router.push(newUrl);
+  };
+
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
   };
 
+  const [token, setToken] = useState(null);
+  const [loginDialog, setLoginDialog] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [confirmLogoutDialog, setConfirmLogoutDialog] = useState(false);
+
+  const handleLogin = async () => {
+    if (email && password) {
+      setLoading(true);
+      const response = await fetch(
+        `${process.env.BASE_URL}/api/Auth/create-token`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setToken(data.data.access_token);
+        localStorage.setItem("crane-user", email);
+        localStorage.setItem("crane-token", data.data.access_token);
+      } else {
+        const data = await response.json();
+        setError(data.message || "Login failed");
+      }
+      setLoading(false);
+      setLoginDialog(false);
+    }
+  };
+
+  const handleLogout = () => {
+    setToken(null);
+    localStorage.setItem("crane-user", "");
+    localStorage.setItem("crane-token", "");
+    setConfirmLogoutDialog(false);
+  };
+
+  const handleAuthClick = () => {
+    if (token) {
+      setConfirmLogoutDialog(true);
+    } else {
+      setLoginDialog(true);
+    }
+  };
+
+  useEffect(() => {
+    const craneToken = localStorage.getItem("crane-token") || "";
+    const craneUser = localStorage.getItem("crane-user") || "";
+    if (craneToken) {
+      setToken(craneToken);
+      setEmail(craneUser);
+    }
+  }, []);
+
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-      <List>
-        {["Asite Form Mapping"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton onClick={handleMapping}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
+    <Box
+      sx={{
+        width: 250,
+        height: "100%", // Drawer yüksekliğini kapla
+        display: "flex",
+        flexDirection: "column", // içerikleri dikey hizala
+        justifyContent: "space-between", // üst menüler yukarıda, login aşağıda
+      }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+    >
+      <Box>
+        <List>
+          {["Asite Form Mapping"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton onClick={handleMapping}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["E-Signature"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton onClick={handleEsignature}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["Crane Form Template"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton onClick={handleCraneFormTemplate}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["Project Cloner"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton onClick={handleProjectCloner}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+      <Box>
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton onClick={handleAuthClick}>
+              <ListItemIcon></ListItemIcon>
+              <ListItemText primary={token ? email : "Crane Login"} />
+              {!token && <LoginIcon />}
             </ListItemButton>
           </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["E-Signature"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton onClick={handleEsignature}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["Crane Form Template"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton onClick={handleCraneFormTemplate}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+        </List>
+      </Box>
     </Box>
   );
 
@@ -110,6 +221,52 @@ export default function DashboardLayout({ children }) {
       <Drawer open={open} onClose={toggleDrawer(false)}>
         {DrawerList}
       </Drawer>
+
+      <Dialog open={loginDialog} onClose={() => setLoginDialog(false)}>
+        <DialogTitle>Giriş Yap</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Kullanıcı Adı"
+            fullWidth
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            margin="dense"
+            label="Şifre"
+            type="password"
+            fullWidth
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setLoginDialog(false)} disabled={loading}>
+            Vazgeç
+          </Button>
+          <Button variant="contained" onClick={handleLogin} disabled={loading}>
+            Giriş Yap
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog
+        open={confirmLogoutDialog}
+        onClose={() => setConfirmLogoutDialog(false)}
+      >
+        <DialogTitle>Çıkış Yap</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Oturum sonlandırılsın mı?</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setConfirmLogoutDialog(false)}>Hayır</Button>
+          <Button variant="contained" onClick={handleLogout}>
+            Evet
+          </Button>
+        </DialogActions>
+      </Dialog>
 
       <Box
         component="main"
